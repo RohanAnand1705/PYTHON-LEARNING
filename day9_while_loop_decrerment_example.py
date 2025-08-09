a02213c116e85ba41bc0790d5fdb6f7f3976b818
ROHAN ANAND <anandrohan1705@gmail.com>
2025-08-09 23:40:43 +0530

    Add files via upload

---
+++ day9_while_loop_decrerment_example.py
@@ -0,0 +1,12 @@
+# i=int(input("Enter number: "))
+# while(i<75):
+#  i=int(input("Enter number: "))
+#  print(i+1)
+# print("End program")
+
+number=int(input("enter:  "))
+while (number>5):
+    print(number)
+    number= number - 2
+else:
+     print("end")
