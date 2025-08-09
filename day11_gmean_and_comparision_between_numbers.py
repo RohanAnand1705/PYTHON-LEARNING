2713f4709a42903c52b2a972685d906e86f7a33a
ROHAN ANAND <anandrohan1705@gmail.com>
2025-08-09 23:48:11 +0530

    Add files via upload

---
+++ day11_gmean_and_comparision_between_numbers.py
@@ -0,0 +1,18 @@
+# def Gmean(a, b):
+#     mean= (a*b)/(a+b)
+#     return(mean)
+
+# a=23
+# b=32
+# print(Gmean(a, b))
+ 
+def k(a, b):
+    if a>b:
+        print(" wrong number")
+    # else:
+    #     print(" correct number")
+    if a<b:
+        pass
+a=int((input( )))
+b=int((input( )))
+k(a, b)
