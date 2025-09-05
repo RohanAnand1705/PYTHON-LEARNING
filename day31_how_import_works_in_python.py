c691f0ddd4bae9eb8baac32a7dd8b1dc2f010525
ROHAN ANAND <anandrohan1705@gmail.com>
2025-09-05 22:25:44 +0530

    Add day 31

---
+++ day31_how_import_works_in_python.py
@@ -0,0 +1,14 @@
+# # # import math
+# from math import pi, sqrt as s  #as is only used for ease
+# result= s(9)*pi
+# print(result)
+# from math import *            #   * imports everything 
+# result=sqrt(9)*pi     
+
+# ''' Though this is not recommended 'cause
+#                         if pi is also in another function a it will
+#                         create confusion'''
+
+# print(result)
+import math as m
+print(dir(m))           #for example, here dir prints all functions in math 
