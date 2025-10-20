be7467edfddae2dc17541fbc52b48ea6db1a994e
ROHAN ANAND <anandrohan1705@gmail.com>
2025-10-21 00:30:07 +0530

    add

---
+++ day68_shutil_module.py
@@ -0,0 +1,10 @@
+import shutil
+import os
+# shutil.copy("day68_shutil_module.py", "Dumbguy.py")
+# shutil.copytree(".sleeping","walking")
+# shutil.move("walking/Dumbguy.py", "Dumbguy.py")
+# shutil.rmtree("walking")
+
+# os.remove(".sleeping/Dumbguy.py")
+# shutil.rmtree(".sleeping")
+os.remove("Dumbguy.py")
